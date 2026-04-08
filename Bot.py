1ac5db6a63e8d7219639b16bdad3804300aae917
zbizuhun28-dot <zbizuhun28@gmail.com>
2026-04-08 03:56:11 +0100

    Implement Telegram bot to delete join messages

---
+++ Bot.py
@@ -0,0 +1,20 @@
+from telegram import Update
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+
+TOKEN = "YOUR_BOT_TOKEN_HERE"  # Replace with your token from BotFather
+
+def delete_joins(update: Update, context: CallbackContext):
+    if update.message.new_chat_members:
+        # Delete the join message
+        context.bot.delete_message(chat_id=update.message.chat.id,
+                                   message_id=update.message.message_id)
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, delete_joins))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
